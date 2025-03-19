9d5262ce0bef511fd741adcc9f2388474c143493
Paxton Clark <paxtonclark23@gmail.com>
2025-03-19 15:26:18 -0400

    Create comboBoxes.java

---
+++ LearningJava/JavaGUI/BroCodeYT/comboBoxes.java
@@ -0,0 +1,50 @@
+package com.mycompany.mavenproject11_unitconversion2_pc;
+
+import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+
+/**
+ *
+ * @author Paxton.Clark
+ */
+public class comboBoxes extends JFrame implements ActionListener{
+    
+    JComboBox comboBox;
+    
+    
+    comboBoxes(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+        
+        Integer[] numbers = {1,2,3};
+        String[] animals = {"Dog", "Cat", "Bird"};
+        comboBox = new JComboBox(animals);  //can also user numbers array
+        comboBox.addActionListener(this);
+        
+        //comboBox.setEditable(true);
+        //System.out.println(comboBox.getItemCount());
+        //comboBox.addItem("Horse");
+        //comboBox.insertItemAt("Pig", 0);
+        //comboBox.setSelectedIndex(0);
+        //comboBox.removeItem("Cat");
+        //comboBox.removeItemAt(0);
+        //comboBox.removeAllItems();
+        
+        this.add(comboBox);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==comboBox){
+            System.out.println(comboBox.getSelectedItem());
+            //System.out.println(comboBox.getSelectedIndex());
+        }
+
+        //throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
