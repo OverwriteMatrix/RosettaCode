13880b27c174b8256843d2c0a5d9707cf673a3f1
Paxton Clark <paxtonclark23@gmail.com>
2025-03-19 15:30:46 -0400

    Create newWindow.java

---
+++ LearningJava/JavaGUI/BroCodeYT/newWindow.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject11_unitconversion2_pc;
+
+import java.awt.Font;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Paxton.Clark
+ */
+public class newWindow {
+    
+    JFrame frame = new JFrame();
+    JLabel label = new JLabel("Hello");
+    
+    
+    
+    newWindow(){
+        
+        label.setBounds(0, 0, 100, 50);
+        label.setFont(new Font(null, Font.PLAIN, 25));
+        frame.add(label);
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+    
+}
